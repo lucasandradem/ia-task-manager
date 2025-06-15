6cb27da7a5eabfab80db61e5e5e72efdcc81dd36
Lucas Mendonça <mendonca.lucas91@gmail.com>
2025-06-15 12:31:09 -0300

    feat: create react router app

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("layouts/layout.tsx", [
+    index("routes/dashboard.tsx"),
+    route("tasks", "routes/tasks.tsx"),
+    route("tasks/new", "routes/task-new.tsx"),
+    route("tasks/edit/:id", "routes/task-edit.tsx"),
+  ]),
+] satisfies RouteConfig;
